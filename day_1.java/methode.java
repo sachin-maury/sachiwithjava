b738bfcb28447dd61ff26981138e9999ec64071f
Sachin Maurya <kumarsachin67671@gmail.com>
2024-01-17 10:30:31 +0530

    first commit

---
+++ day_1.java/methode.java
@@ -0,0 +1,14 @@
+
+
+public class methode {
+    public static void main(String[] args) {
+      int [] marks = {57 ,58 ,87 ,98 , 45};
+     // for(int i=0; i<marks.length; i++){
+      //  System.out.println(marks[i]);
+      for(int element:marks){
+        System.out.println(element);
+      }
+      
+    }
+    
+}
---
+++ max_value.java
@@ -0,0 +1,24 @@
+/**
+ * max_value
+ */
+public class max_value {
+
+    public static void main(String[] args) {
+        int arr[]={12,34,56,78,90};
+        int n=arr.length;
+        check sc=new check();
+    System.out.println(sc.largest(arr,n));
+     
+    }
+}
+ class check{
+     static int  largest(int arr[],int n){
+        int max=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+            max=arr[i];
+            }
+        }
+        return max;
+    }
+}
