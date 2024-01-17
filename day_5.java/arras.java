612022804858ae2fc85eb7a472feb914c5a7d253
Sachin Maurya <kumarsachin67671@gmail.com>
2024-01-17 11:44:02 +0530

    Array qn

---
+++ day_5.java/arras.java
@@ -0,0 +1,12 @@
+
+
+public class arras {
+    public static void main(String[] args) {
+        float [] marks ={55.8f ,45.8f ,78.9f ,69.4f ,55.9f};
+        float sum=0;
+        for(float number:marks){
+            sum=sum+number;
+        }
+        System.out.println(sum);
+    }
+}
---
+++ day_5.java/array.java
@@ -0,0 +1,20 @@
+
+
+public class array {
+    public static void main(String[] args) {
+        float [] marks = new float[5];
+        marks [0]=48.5f;
+        marks [1]=54.5f;
+        marks [2]=65.5f;
+        marks [3]=45.5f;
+        marks [4]=55.5f;
+        System.out.println(marks [0]+marks [1]+marks [2]+marks [3]+marks [4]); 
+     /*  float [] marks = {54.5f ,64.5f ,56.5f ,69.5f};
+        
+        System.out.println(marks.length);
+        System.out.println(marks [2]);*/  
+    
+
+        
+    }
+}
