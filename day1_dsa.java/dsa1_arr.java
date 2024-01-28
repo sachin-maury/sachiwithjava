7f7250cf62810931fb6fe35cdf47346905073091
Sachin Maurya <kumarsachin67671@gmail.com>
2024-01-28 16:50:30 +0530

    dd

---
+++ day1_dsa.java/dsa1_arr.java
@@ -0,0 +1,28 @@
+
+public class dsa1_arr {
+    public static void main (String [] args){
+     // 1st methode to create array:
+     /*        int number[] =new int[5];
+          number[0]=50;
+          number[1]=14;
+          number[2]=48;
+          number[3]=32;
+          number[4]=76;
+          System.out.println(number[4]); */
+          // 2nd methode to create array:
+      /*      int values [] ={45,22,24,67,87,90};
+           for(int total:values){
+            System.out.println(total); 
+           } */
+      // 3rd method to create array:
+      
+      int morevalues [];
+      morevalues=new int[6];
+      morevalues [0]=54;
+      morevalues [1]=45;
+      morevalues [3]=63;
+      morevalues [4]=55;
+      morevalues [5]=98; 
+      System.out.println(morevalues[3]);
+    }
+}
