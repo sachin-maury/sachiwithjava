d33bd57bb072a3e31133f30af720a38da1fcd3c8
Sachin Maurya <kumarsachin67671@gmail.com>
2024-02-01 23:50:10 +0530

    DSA I/O problem

---
+++ day1_dsa.java/dsa2_arry.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class dsa2_arry {
+    public static void main(String[] args) {
+        int marks []=new int[100];
+        Scanner sc =new Scanner(System.in);
+        marks [0] =sc.nextInt();
+        marks [1] =sc.nextInt();
+        marks [2] =sc.nextInt();
+        System.out.println("physics :"+ marks [0]);
+        System.out.println("chemistry :"+ marks [1]);
+        System.out.println("math :"+ marks [2]);
+
+    }
+}
