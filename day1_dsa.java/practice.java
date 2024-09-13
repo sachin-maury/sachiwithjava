64d6ebee9e4f2b7d54f83fb2b4ca74393086d77c
Sachin Maurya <kumarsachin67671@gmail.com>
2024-09-13 21:50:21 +0530

    odd even

---
+++ day1_dsa.java/practice.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class practice {
+    public static void main(String[] args) {
+        System.out.println("Enter your number");
+      Scanner sc = new Scanner(System.in);
+      int num =sc.nextInt();
+      
+      if(num % 2 == 0){
+        System.out.println("this is a sum number "+num);
+
+      } 
+      else{
+        System.out.println("this is a odd number"+num);
+      }
+    }
+}
