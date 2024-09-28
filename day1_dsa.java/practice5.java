0634c5e98ff9d376dbb2a2a19b5ac14e8158d4f9
Sachin Maurya <kumarsachin67671@gmail.com>
2024-09-28 22:38:35 +0530

    hello

--- day1_dsa.java/practice5.java
+++ day1_dsa.java/practice5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class practice3 {
+public class practice5 {
     public static void main(String[] args) {
         System.out.println("Enter your number");
       Scanner sc = new Scanner(System.in);
